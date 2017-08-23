f39bae2c2c39b4d678149a28c5bd38f0fa877096
代祥 <daixiang@Daixiang.local>
2017-08-23 10:52:44 +0800

    Support the NDK SDK

--- rokid-openvoice-process-android-app/jni/Android.mk
+++ rokid-openvoice-process-android-app/jni/Android.mk
@@ -7,25 +7,26 @@ include $(call all-subdir-makefiles)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libopenvoice
 LOCAL_SRC_FILES := \
-		src/audio_recorder.cpp \
-        src/opensl_io.c \
-        src/VoiceService.cpp \
-        src/VoiceCallback.cpp
+		main/audio_recorder.cpp \
+		main/mic_array.c \
+        main/VoiceService.cpp \
+        main/VoiceCallback.cpp
     
 LOCAL_SHARED_LIBRARIES := \
         libbsiren \
+		libtinyalsa \
         libspeech
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_LDLIBS := -llog -lOpenSLES
-LOCAL_CFLAGS += -std=c++11
+LOCAL_CPPFLAGS += -std=c++11
 include $(BUILD_SHARED_LIBRARY)
 
 #===========================================
 #
 include $(CLEAR_VARS)
 LOCAL_MODULE := libopenvoice2
-LOCAL_SRC_FILES := src/com_rokid_openvoice_VoiceNative.cpp
+LOCAL_SRC_FILES := main/com_rokid_openvoice_VoiceNative.cpp
         
 LOCAL_LDLIBS := -llog
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
---
+++ rokid-openvoice-process-android-app/jni/main/com_rokid_openvoice_VoiceNative.cpp
@@ -0,0 +1,43 @@
+#include <jni.h>
+#include "VoiceService.h"
+
+shared_ptr<VoiceService> service = make_shared<VoiceService>();
+
+extern "C" {
+
+JNIEXPORT jboolean JNICALL 
+Java_com_rokid_openvoice_VoiceNative_setup(JNIEnv *env, jclass)
+{
+	LOGD("%s", __FUNCTION__);
+    return service->setup();
+}
+
+JNIEXPORT void JNICALL 
+Java_com_rokid_openvoice_VoiceNative_startSiren(JNIEnv *env, jclass, jboolean isopen)
+{
+	LOGD("%s", __FUNCTION__);
+	service->start_siren((bool)isopen);
+}
+
+JNIEXPORT void JNICALL 
+Java_com_rokid_openvoice_VoiceNative_setSirenState(JNIEnv *env, jclass, jint state)
+{
+	LOGD("%s", __FUNCTION__);
+	service->set_siren_state((int)state);
+}
+
+JNIEXPORT void JNICALL 
+Java_com_rokid_openvoice_VoiceNative_networkStateChange(JNIEnv *env, jclass, jboolean isconnect)
+{
+	LOGD("%s", __FUNCTION__);
+	service->network_state_change((bool)isconnect);
+}
+
+JNIEXPORT void JNICALL 
+Java_com_rokid_openvoice_VoiceNative_updateStack(JNIEnv *env, jclass, jstring appid)
+{
+	LOGD("%s", __FUNCTION__);
+    service->update_stack(string(env->GetStringUTFChars(appid, NULL)));
+}
+
+}
