2ea4793ffcbc836879e529cabd9d3a7976d78d47
代祥 <daixiang@Daixiang.local>
2017-08-23 15:01:58 +0800

    update

--- rokid-openvoice-process-android-app/jni/Android.mk
+++ rokid-openvoice-process-android-app/jni/Android.mk
@@ -17,9 +17,14 @@ LOCAL_SHARED_LIBRARIES := \
 		libtinyalsa \
         libspeech
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES := \
+		$(LOCAL_PATH)/include \
+		$(LOCAL_PATH)/blacksiren/libbsiren/include \
+		$(LOCAL_PATH)/blacksiren/libjsonc/include
+
 LOCAL_LDLIBS := -llog -lOpenSLES
 LOCAL_CPPFLAGS += -std=c++11
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 include $(BUILD_SHARED_LIBRARY)
 
 #===========================================
@@ -29,7 +34,6 @@ LOCAL_MODULE := libopenvoice2
 LOCAL_SRC_FILES := main/com_rokid_openvoice_VoiceNative.cpp
         
 LOCAL_LDLIBS := -llog
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_SHARED_LIBRARIES := libopenvoice
 LOCAL_CFLAGS += -std=c++11
    
