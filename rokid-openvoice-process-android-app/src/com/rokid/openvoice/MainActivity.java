2a9cf071bbbf315250a58d69f822416297fe1649
wb_xiang.dai <wb_xiang.dai@rokid.com>
2017-09-02 20:30:19 +0800

    update

--- rokid-openvoice-process-android-app/src/com/rokid/openvoice/MainActivity.java
+++ rokid-openvoice-process-android-app/src/com/rokid/openvoice/MainActivity.java
@@ -1,5 +1,7 @@
 package com.rokid.openvoice;
 
+
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,8 +11,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        VoiceNative.setup();
-        VoiceNative.networkStateChange(true);
+        VoiceManager.setup();
+        VoiceManager.networkStateChange(true);
     }
 }
 //09-02 09:18:51.549: W/speech.Connection(7327): websocket connect failed: No address found: apigwws.open.rokid.com, N4Poco3Net23NoAddressFoundExceptionE
